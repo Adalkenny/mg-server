0cf126f42cdf8ec358e69091d7a7399575b1440d
Adalberto Mascarenhas <adalbertom@caixa.cv>
2023-02-18 13:58:01 -0100

    MG Spring Boot project setup

---
+++ src/main/java/cv/mentex/mgserver/MgServerApplication.java
@@ -0,0 +1,13 @@
+package cv.mentex.mgserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MgServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MgServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cv/mentex/mgserver/MgServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cv.mentex.mgserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MgServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
